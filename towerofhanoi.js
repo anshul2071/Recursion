3a06067b2876f92c86072b8bc3b8cc594bb21a04
Anshul Rawal <anshul2071@gmail.com>
2025-02-19 13:07:30 +0545

    final commit

--- towerofhanoi.js
+++ towerofhanoi.js
@@ -1,7 +1,7 @@
-function towerOfHanoi (n, from, to, intmd) {
+function towerOfHanoi (n, src, dest, intmd) {
 
 if(n === 1) {
-    console.log(`Move disk 1 from ${from} to ${to}`);
+    console.log(`Move disk 1 from ${src} to ${dest}`);
    
 
 }
@@ -11,13 +11,13 @@ else {
     if(n>1)
 
         {
-            towerOfHanoi(n - 1, from, intmd, to);
+            towerOfHanoi(n - 1, src, intmd, dest);
         }
 
-console.log(`Move disk ${n} from ${from} to ${to}`);
+console.log(`Move disk ${n} from ${src} to ${dest}}`);
 
 if(n>1){
-    towerOfHanoi(n - 1, intmd, to , from);
+    towerOfHanoi(n - 1, intmd, dest , src);
 
    }
 
